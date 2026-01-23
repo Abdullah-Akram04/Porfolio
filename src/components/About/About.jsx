1a3167707b274357006d1ce8e10a0ed8eabe69a4
Abdullah Akram <your-email@example.com>
2026-01-23 08:17:02 +0500

    Circular

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -65,7 +65,7 @@ const About = () => {
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
           <Tilt
-            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            className="flex items-center justify-center w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 border-4 border-purple-700 rounded-full overflow-hidden"
             tiltMaxAngleX={20}
             tiltMaxAngleY={20}
             perspective={1000}
@@ -76,7 +76,7 @@ const About = () => {
             <img
               src={profileImage}
               alt="Abdullah Akram"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+              className="w-full h-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
         </div>
