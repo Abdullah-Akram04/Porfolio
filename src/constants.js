b66dd7262351f238ada66d48f1c7b88a08c0223b
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-02-02 19:39:37 +0500

    Html removed from constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // Skills Section Logo's
-import htmlLogo from './assets/tech_logo/html.png';
+
 import cssLogo from './assets/tech_logo/css.png';
 import javascriptLogo from './assets/tech_logo/javascript.png';
 import reactjsLogo from './assets/tech_logo/reactjs.png';
@@ -53,7 +53,6 @@ export const SkillsInfo = [
   {
     title: 'Frontend',
     skills: [
-      { name: 'HTML', logo: htmlLogo },
       { name: 'CSS', logo: cssLogo },
       { name: 'JavaScript', logo: javascriptLogo },
       { name: 'React JS', logo: reactjsLogo },
