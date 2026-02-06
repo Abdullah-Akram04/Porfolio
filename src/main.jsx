bbcdd31af4ac183f016f65c12cf91298c6a239a2
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-02-06 19:26:07 +0500

    update main.jsx and About.jsx with new content

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-// src/main.jsx
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Main entry point for the React application
 createRoot(document.getElementById('root')).render(
   
   <App />
