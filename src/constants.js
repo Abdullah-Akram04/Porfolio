fce80fdbd60d63806e8d48ef088de4466d5003cf
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-02-14 20:28:46 +0500

    collecting commit message...

--- src/constants.js
+++ src/constants.js
@@ -163,7 +163,7 @@ export const SkillsInfo = [
       id: 1,
       img: bsaLogo,
       school: "Punjab group of colleges",
-      date: "Sept 2018 - Aug 2021",
+      date: "Sept 2020 - Aug 2022",
       grade: "73.2%",
       desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
       degree: "FSC Pre-Engineering",
