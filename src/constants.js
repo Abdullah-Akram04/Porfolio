a22c8c2501fcc207324ff44be800f0b17207d9e2
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-03-04 22:52:04 +0500

    replace with a meaningful commit message

--- src/constants.js
+++ src/constants.js
@@ -177,7 +177,7 @@ export const SkillsInfo = [
       description:
         "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
       image: csprepLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "CSS", "JavaScript"],
+      tags: ["React JS", "Node.js", "MongoDB", "CSS", "JavaScript"],
       github: "https://github.com/codingmastr/CSPrep",
       webapp: "https://csprep.netlify.app/",
     },
