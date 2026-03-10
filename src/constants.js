e98963344321ac93876ab7e38be6e14163a7cdfb
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-03-10 20:47:56 +0500

    update constants.js

--- src/constants.js
+++ src/constants.js
@@ -273,3 +273,5 @@ export const SkillsInfo = [
       desc: "I have earned the ISC2 Certified in Cybersecurity (CC) certification, demonstrating my foundational knowledge and skills in cybersecurity principles and practices. This certification validates my understanding of key cybersecurity concepts, including risk management, network security, and incident response, showcasing my commitment to maintaining a secure digital environment.",
     },
   ];  
+
+  
