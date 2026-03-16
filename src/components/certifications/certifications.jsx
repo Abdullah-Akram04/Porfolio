f2623808e63dcb6d18ffc28baa42a8828e7841a7
Abdullah Akram <ch.abdullahakram786@gmail.com>
2026-03-16 19:55:17 +0500

    certifications --- IGNORE ---

--- src/components/certifications/certifications.jsx
+++ src/components/certifications/certifications.jsx
@@ -11,7 +11,7 @@ const Certifications = () => {
         <h2 className="text-4xl font-bold text-white">CERTIFICATIONS</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
         <p className="text-gray-400 mt-4 text-lg font-semibold">
-          My certifications showcase my commitment to continuous learning and professional development
+          My  showcase my commitment to continuous learning and professional development
         </p>
       </div>
 
